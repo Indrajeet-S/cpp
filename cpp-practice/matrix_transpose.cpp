54db5b9c506f9c5b7ede89a5477270e5d5c181a6
Indrajeet Shelake <indravshelake95@gmail.com>
2021-11-21 20:16:33 +0530

    transpose of a matrix code written
    check again and take dry run

--- cpp-practice/matrix_transpose.cpp
+++ cpp-practice/matrix_transpose.cpp
@@ -3,17 +3,40 @@ using namespace std;
 
 int main () {
 
+// size of array matrix
 int n;
 cin>>n;
 
+// declare array matrix
 int A[n][n];
 
+// input array elements of matrix
 for (int i=0;i<n;i++)
 {
     for(int j=0;j<n;j++)
     {
         cin>>A[i][j];
     }
+}
+
+// declare transpose array matrix
+int A_trans[n][n];
+
+// transpose matrix code
+
+cout<<"transpose of given matrix is "<<endl;
+
+for (int j = 0; j < n; j++)
+{
+    for (int i = 0; i < n; i++)
+    {
+        A_trans[j][i]=A[i][j];
+
+        // print transpose array element 
+        cout<<A_trans[j][i]<<" "; 
+    }
+    cout<<endl;
+    
 }
 
 
